497c51c794fbb568f7398936815d5a95fd8a53e4
AndyKodehode <andykodehode@gmail.com>
2022-03-31 14:37:24 +0200

    update new pc

---
+++ script.js
@@ -0,0 +1,67 @@
+//IMAGE SLIDER FRONT-PAGE
+let body = document.querySelector("body")
+
+let titleCont = document.querySelectorAll(".title-cont")
+let kahootImg = document.querySelector("#kahoot-img-cont")
+let streamingImg = document.querySelector("#streaming-img-cont")
+let fromRight = document.querySelectorAll(".from-right")
+let fromLeft = document.querySelectorAll(".from-left")
+let designCont = document.querySelectorAll(".design-cont")
+
+
+
+
+
+//RIGHT
+const targetRight = document.querySelectorAll(['#kahoot-img-cont', '.css-cont', '.title-cont'])
+
+let observerRight = new IntersectionObserver(entries => {
+  entries.forEach( entry => {
+    
+  
+    if (entry.isIntersecting) {
+      entry.target.classList.add('a-right')
+      
+
+    } else {
+      entry.target.classList.remove('a-right')
+    }
+  });
+})
+
+
+
+
+
+targetRight.forEach( targetRight =>{
+observerRight.observe(targetRight);
+})
+
+//LEFT
+
+const targetLeft = document.querySelectorAll(['#streaming-img-cont', '#calc-img-cont']);
+
+
+let observerLeft = new IntersectionObserver(entries => {
+  entries.forEach( entry => {
+    
+  
+    if (entry.isIntersecting) {
+      entry.target.classList.add('a-left')
+      
+
+    } else {
+      entry.target.classList.remove('a-left')
+    }
+  });
+})
+
+
+
+
+
+targetLeft.forEach( targetLeft =>{
+observerLeft.observe(targetLeft);
+})
+
+
