8aa1e76ee80af7e0f541c8f953c718abc61922a9
AndyKodehode <andykodehode@gmail.com>
2022-09-20 12:31:38 +0200

    carousel edit

--- design.js
+++ design.js
@@ -5,6 +5,8 @@ let imgCont = document.querySelectorAll(".img-cont")
 let slides = document.querySelectorAll(".single-img-div")
 let img = document.querySelector(".img-circle");
 let arrow = document.querySelector(".arrow")
+let circleArrowDown= document.querySelector(".circle-arrow-down")
+let circleArrowUp= document.querySelector(".circle-arrow-up")
 let dots = document.getElementsByClassName("small-circle");
 let textCont = document.querySelectorAll(".text-cont")
 let text = document.querySelectorAll(".link")
@@ -13,7 +15,7 @@ let slidePosition = 1
 const totalSlides = slides.length;
 
 imgCont.forEach(function(){
-  arrow.addEventListener("click", function()
+  circleArrowUp.addEventListener("click", function()
    {
    for( let i=0; i< slides.length; i++)
 
@@ -48,7 +50,7 @@ let textPosition = 1
 const textSlides = text.length;
 
 textCont.forEach(function(){
-  arrow.addEventListener("click", function()
+  circleArrowUp.addEventListener("click", function()
    {
    for( let i=0; i< text.length; i++)
 
@@ -78,3 +80,35 @@ textCont.forEach(function(){
   )
 
 })
+
+
+
+/*imgCont.forEach(function(){
+  circleArrowUp.addEventListener("click", function()
+   {
+   for( let i=1; i < slides.length; i++)
+
+   slides.forEach(function()
+   {
+   slides[i].classList.add("active")
+   slides[slidePosition].classList.remove("active")
+  
+  
+  }
+   
+   )
+   
+
+   if (slidePosition === totalSlides - 1) {
+    slidePosition = 0
+   } else {
+    slidePosition++
+   }
+
+  
+   console.log(slides)
+  }
+
+  )
+
+})*/
